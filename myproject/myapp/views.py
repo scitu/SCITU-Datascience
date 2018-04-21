ab64bc19c6e940fb9150fa77d06045b6daaedc43
Wasit Limprasert <wasit7@gmail.com>
2018-04-22 00:51:10 +0700

    test git config

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-	return render(request, 'home.html', {'key': "value" })
+	return render(request, 'home.html', {'key': "value" }) 
